4cf81f47086ac18ff36b51bdb45cd482e3ac7452
S. Chris Colbert <sccolbert@gmail.com>
2015-09-05 14:29:07 -0400

    update to TS 1.6

--- scripts/dtsbundle.js
+++
@@ -1,7 +0,0 @@
-require('dts-generator').generate({
-  name: 'phosphor-splitpanel',
-  main: 'phosphor-splitpanel/index',
-  baseDir: 'lib',
-  files: ['index.d.ts'],
-  out: 'lib/phosphor-splitpanel.d.ts',
-});
