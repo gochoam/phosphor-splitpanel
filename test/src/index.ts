f796d37d09a521f269ee62f498507b0fa46551ab
Steven Silvester <steven.silvester@ieee.org>
2015-09-23 11:40:56 -0500

    wip add tests

--- test/src/index.ts
+++ test/src/index.ts
@@ -10,11 +10,77 @@
 import expect = require('expect.js');
 
 
+import {
+  HIDDEN_CLASS, HORIZONTAL_CLASS, OVERLAY_CLASS, SPLIT_HANDLE_CLASS,
+  SPLIT_PANEL_CLASS, VERTICAL_CLASS, Orientation, SplitPanel
+} from '../../lib/index';
+
 describe('phosphor-splitpanel', () => {
 
-  describe('testing stub', () => {
 
-    it('should always pass', () => {
+  describe('HIDDEN_CLASS', () => {
+
+    it('should equal `p-mod-hidden`', () => {
+      expect(HIDDEN_CLASS).to.be('p-mod-hidden');
+    });
+
+  });
+
+  describe('HORIZONTAL_CLASS', () => {
+
+    it('should equal `p-mod-horizontal`', () => {
+      expect(HORIZONTAL_CLASS).to.be('p-mod-horizontal');
+    });
+
+  });
+
+  describe('OVERLAY_CLASS', () => {
+
+    it('should equal `p-SplitHandle-overlay`', () => {
+      expect(OVERLAY_CLASS).to.be('p-SplitHandle-overlay');
+    });
+
+  });
+
+  describe('SPLIT_HANDLE_CLASS', () => {
+
+    it('should equal `p-SplitHandle`', () => {
+      expect(SPLIT_HANDLE_CLASS).to.be('p-SplitHandle');
+    });
+
+  });
+
+  describe('SPLIT_PANEL_CLASS', () => {
+
+    it('should equal `p-SplitPanel`', () => {
+      expect(SPLIT_PANEL_CLASS).to.be('p-SplitPanel');
+    });
+
+  });
+
+  describe('VERTICAL_CLASS', () => {
+
+    it('should equal `p-mod-vertical`', () => {
+      expect(VERTICAL_CLASS).to.be('p-mod-vertical');
+    });
+
+  });
+
+  describe('SplitPanel', () => {
+
+    describe('.Horizontal', () => {
+
+      it('should be an alias of the `Horizontal` Orientation', () => {
+          expect(SplitPanel.Horizontal).to.be(Orientation.Horizontal);
+      });
+
+    });
+
+    describe('.Vertical', () => {
+
+      it('should be an alias of the `Vertical` Orientation', () => {
+          expect(SplitPanel.Vertical).to.be(Orientation.Vertical);
+      });
 
     });
 
